5a67d64509f413cf250b107d960c35c3dd531c2a
Federico Cardozo <fedecar94@outlook.com>
2021-11-03 20:14:00 -0300

    se agregaron las validaciones en el endpoint de uso

--- puntos/serializers.py
+++ puntos/serializers.py
@@ -1,4 +1,7 @@
-from rest_framework.serializers import ModelSerializer
+from django.db.models import Sum
+from django.utils import timezone
+
+from rest_framework.serializers import ModelSerializer, Serializer, IntegerField, ValidationError
 
 from .models import (Asignacion, Bolsa, Cliente, Concepto, Uso, UsoDetalle,
                      Vencimiento)
@@ -10,15 +13,17 @@ class AsignacionSerializer(ModelSerializer):
         fields = '__all__'
 
 
-class BolsaSerializer(ModelSerializer):
-    class Meta:
-        model = Bolsa
-        fields = '__all__'
-
-
 class ClienteSerializer(ModelSerializer):
     class Meta:
         model = Cliente
+        fields = '__all__'
+
+
+class BolsaSerializer(ModelSerializer):
+    cliente = ClienteSerializer()
+
+    class Meta:
+        model = Bolsa
         fields = '__all__'
 
 
@@ -42,6 +47,28 @@ class UsoSerializer(ModelSerializer):
     class Meta:
         model = Uso
         fields = '__all__'
+
+    def validate(self, attrs):
+        concepto = attrs.get('concepto')
+        p_utilizado = attrs.get('p_utilizado')
+        cliente = attrs.get('cliente')
+
+        if p_utilizado < concepto.puntos_requerido:
+            raise ValidationError({
+                'p_utilizado': 'No son suficientes puntos para el concepto'
+            })
+
+        if p_utilizado > cliente.bolsas.filter(vencimiento__gte=timezone.now(), saldo__gt=0).aggregate(total=Sum('saldo'))['total']:
+            raise ValidationError({
+                'p_utilizado': 'El cliente no posee suficientes puntos'
+            })
+
+        return attrs
+
+
+class CalularPuntosSerializer(Serializer):
+    monto = IntegerField(default=0)
+    equivale = IntegerField(read_only=True)
 
 
 class VencimientoSerializer(ModelSerializer):
--- puntos/views.py
+++ puntos/views.py
@@ -1,8 +1,10 @@
+import math
 from datetime import timedelta
 from math import floor
 
 from django.db import transaction
 from django.utils import timezone
+from rest_framework.decorators import action
 from rest_framework.mixins import ListModelMixin
 from rest_framework.response import Response
 from rest_framework.viewsets import GenericViewSet, ModelViewSet
@@ -11,7 +13,7 @@ from .models import (Asignacion, Bolsa, Cliente, Concepto, Uso, UsoDetalle,
                      Vencimiento)
 from .serializers import (AsignacionSerializer, BolsaSerializer,
                           ClienteSerializer, ConceptoSerializer, UsoSerializer,
-                          VencimientoSerializer)
+                          VencimientoSerializer, CalularPuntosSerializer)
 
 
 class AsignacionViewSet(ModelViewSet):
@@ -55,6 +57,11 @@ class UsoViewSet(GenericViewSet, ListModelMixin):
     queryset = Uso.objects.all()
     serializer_class = UsoSerializer
 
+    def get_serializer_class(self):
+        if self.action == 'calcular':
+            return CalularPuntosSerializer
+        return super().get_serializer_class()
+
     def create(self, request, *args, **kwargs):
         serializer = self.get_serializer(data=request.data)
         serializer.is_valid(raise_exception=True)
@@ -63,7 +70,7 @@ class UsoViewSet(GenericViewSet, ListModelMixin):
             now = timezone.now()
             puntos = uso.p_utilizado
             while puntos > 0:
-                bolsa = Bolsa.objects.filter(vencimiento__gte=now, saldo__gt=0).order_by('asignacion').first()
+                bolsa = uso.cliente.bolsas.filter(vencimiento__gte=now, saldo__gt=0).order_by('asignacion').first()
                 menor = puntos if puntos < bolsa.saldo else bolsa.saldo
                 bolsa.p_utilizado += menor
                 bolsa.save()
@@ -74,6 +81,19 @@ class UsoViewSet(GenericViewSet, ListModelMixin):
                 )
                 puntos -= menor
         return Response(self.get_serializer(uso).data, status=201)
+
+    @action(methods=['post'], detail=False)
+    def calcular(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        monto = serializer.data.get('monto')
+
+        asignacion = Asignacion.objects.filter(limite_inferior__lte=monto, limite_superior__gte=monto).first()
+        if not asignacion:
+            asignacion = Asignacion.objects.get(limite_inferior=0, limite_superior=0)
+
+        equivale = math.floor(monto/asignacion.equivalencia)
+        return Response({'monto': monto, 'equivale': equivale}, status=200)
 
 
 class VencimientoViewSet(ModelViewSet):
